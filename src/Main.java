8e34886d6301d14a61e36b1362299d614e64fd9f
laysch <laysch99@gmail.com>
2024-02-20 18:24:20 -0500

    questions: 1. Write a function that prints "Hello World" n times recursively. 2. Write a function that returns the sum of all numbers between n1 and n2 that are multiples of 7 using recursion.  3. Write a function that implements the binary search algorithm recursively

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-
-// question number 1: Write a function that prints "Hello World" n times recursively.
 public class Main {
+// question number 1: Write a function that prints "Hello World" n times recursively.
     public static void printHelloWorld(int n) {
 
         if (n <=0) { //base case: stop when n is less than or equal to 0
@@ -9,10 +8,8 @@ public class Main {
         System.out.println("Hello World");
         printHelloWorld(n - 1);
     }
-}
 
 //question 2: Write a function that returns the sum of all numbers between n1 and n2 that are multiples of 7 using recursion.
-public class Main {
     public static int sumRecursive(int n1, int n2) {
         if (n1 > n2) { //base case: means the range is empty, so it returns 0 (no multiples of 7 exist).
             return 0;
@@ -22,10 +19,8 @@ public class Main {
             return sumRecursive(n1 + 1, n2);
         }
     }
-    }
 
     // question 3: Write a function that implements the binary search algorithm recursively.
-    public class Main {
         public static int binarySearch(int[] arr, int searchInt, int start, int end) {
             if (start > end) {
                 return -1;  // base case: if the search range is empty, the searchInt is not found.
